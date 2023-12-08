5bea7d62433011ecaf86a1a36a09da66ec75464d
sousalmdev <leandromedeiros690@gmail.com>
2023-12-08 13:17:44 -0300

    My 1st Gen Pokédex

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <span class="name">${pokemon.name.toUpperCase()}</span>
 
             <div class="detail">
                 <ol class="types">
